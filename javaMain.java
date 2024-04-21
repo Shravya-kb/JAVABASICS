49d44b15149cb0c4df4be057fa24d91857c7e119
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-04-21 20:23:01 +0530

    Create javaMain.java
    Java's main function name's importance is written in the code.

---
+++ javaMain.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class java{
+public static void main(String args[]){
+System.out.println("Hello lets start testing");
+}
+
+}
+
+// the main function name is compulsory in java. if not written it will give error.
+// Error: Main method not found in class java, please define the main method as:
+  //  public static void main(String[] args)
+// or a JavaFX application class must extend javafx.application.Application
