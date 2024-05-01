bc70396f37134b48554463baf371c751945fd7a7
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-01 14:59:17 +0000

    ForLoops summary

---
+++ ForJavaLoop.java
@@ -0,0 +1,36 @@
+// import java.util.*;
+
+// public class ForJavaLoop{
+//     public static void main(String args[]){
+//         for(i=0;i<10;i++){
+//             System.out.println("Hello World");
+//         }
+//     }
+// }
+
+/*output:
+
+*/
+
+// import java.util.*;
+
+// public class ForJavaLoop{
+//     public static void main(String args[]){
+//         for(int line=1;line<=4;line++){
+//            System.out.println("****") ;
+//         }
+        
+//     }
+// }
+// OUTPUT:
+
+import java.util.*;
+
+public class ForJavaLoop{
+    public static void main(String args[]){
+        for(int line=1;line<=4;line++){
+           System.out.println("****") ;
+        }
+        
+    }
+}
