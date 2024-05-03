2c547dea170d9ca5da65eca236c28da8ae77c2c7
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-03 11:07:54 +0000

    star half pyramid pattern

---
+++ Patterns Part-1/halfPyramidStar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class halfPyramidStar{
+    public static void main(String args[]){
+        System.out.println("Enter n:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int line=1;line<=n;line++)s{
+            System.out.print(" * ");
+        }
+        System.out.println();
+    }
+    
+}
