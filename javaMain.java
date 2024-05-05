48d1ce7e034f5dd4185b0605b9ee0a69fd4ea1d8
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-05 02:59:16 +0000

    MAIN FUNCTION SUMMARY

--- javaMain.java
+++ javaMain.java
@@ -1,10 +1,10 @@
-import java.util.*;
-public class java{
-public static void main(String args[]){
-System.out.println("Hello lets start testing");
-}
+// import java.util.*;
+// public class java{
+// public static void main(String args[]){
+// System.out.println("Hello lets start testing");
+// }
 
-}
+// }
 
 // the main function name is compulsory in java. if not written it will give error.
 // Error: Main method not found in class java, please define the main method as:
