329253f5136a53d8c3572a3ff09a5cefc9b8c17a
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-16 12:46:53 +0000

    functions

--- FunctionandMethods/JavaBa.java
+++ FunctionandMethods/JavaBa.java
@@ -203,25 +203,26 @@ Binomial Coeffient of 2no. is:10
 //OUTPUT:
 //Decimal of100=4
 
-import java.util.*;
-public class JavaBa{
-  public static void decToBin(int n){
-    int myNum = n;
-    int pow=0;
-    int binNum=0;
+// import java.util.*;
+// public class JavaBa{
+//   public static void decToBin(int n){
+//     int myNum = n;
+//     int pow=0;
+//     int binNum=0;
 
-    while(n>0){
-      int rem = n%2;
-      binNum = binNum+(rem*(int)Math.pow(10,pow));
-      pow++;
-      n=n/2;
-    }
-    System.out.println("binary no. of "+myNum +"="+binNum);
-  }
+//     while(n>0){
+//       int rem = n%2;
+//       binNum = binNum+(rem*(int)Math.pow(10,pow));
+//       pow++;
+//       n=n/2;
+//     }
+//     System.out.println("binary no. of "+myNum +"="+binNum);
+//   }
 
 
-  public static void main(String args[]){
-    decToBin(11);
-  }
-}
+//   public static void main(String args[]){
+//     decToBin(11);
+//   }
+// }
 //OUTPUT:binary no. of 11=1011
+
