ef8323e43a64af4798c5edfabfe7788654cee5bc
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-16 09:52:45 +0000

    Functions

--- FunctionandMethods/JavaBasics.java
+++ FunctionandMethods/JavaBasics.java
@@ -79,3 +79,88 @@ Product of 2 no.s is:30
    OUTPUT:
  Factorial is: 24
    */
+/* 
+   import java.util.*;
+   public class functions{
+    public static int factorial(int n){
+      int f=1;
+      for(int i=1;i<=n;i++){
+        f=f*i;
+      }
+      return f;
+
+    }
+    public static int binCoeffient(int n, int r){
+      int num1 =factorial(n);
+       int r1=factorial(r);
+      int nmr=factorial(n-r);
+      int binCoeffient = num1/(nmr*r1);
+      return binCoeffient;
+    }
+    public static void main(String args[]){
+      System.out.println("Enter 2 no.s for binomial Coeffient calculation:");
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int r=sc.nextInt();
+      System.out.println("Binomial Coeffient of 2no. is:"+binCoeffient(n,r));
+    }
+   }
+*/
+   /*
+OUTPUT:
+Enter 2 no.s for binomial Coeffient calculation:
+5
+2
+Binomial Coeffient of 2no. is:10
+ */
+
+// import java.util.*;
+// public class JavaBasics{
+//   public static boolean isPrime(int n) {
+//     if(n==2){
+//       return true;
+//     }
+//     for(int i=2;i<=n-i;i++){
+//       if(n%i==0){
+//         return false;
+//       }
+//     }
+// return true;
+// }
+// public static void main(String args[]){
+//   System.out.println(isPrime(25));
+// }
+// }
+//OUTPUT:
+//false
+
+// FIND PRIME NUMBERS WITHIN GIVEN RANGE:
+
+import java.util.*;
+public class JavaBasics{
+
+
+public static boolean isPrime(int n){
+  
+    if(n==2){
+      return true;
+    }
+    for( int i=2; i<=n-1; i++){
+      if(n%2==0){
+        return false;
+      }
+    }
+    return true;
+}
+
+public static void primeInRange(int n){
+  for(int i=2;i<=n-1;i++){
+    if(isPrime(i)){
+    System.out.println(i+" ");
+    }
+  }
+}
+public static void main(String args[]){
+  primeInRange(20);
+}
+}
