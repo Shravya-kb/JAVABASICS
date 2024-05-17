f7f51bb0f6f7316a296f86d6d561a8e35a20606c
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-17 13:08:53 +0000

    HallowRectangle

---
+++ Advanced Patterns/01_HallowRectangle.java
@@ -0,0 +1,37 @@
+// import java.util.*;
+// public class HallowRectangle{
+//     public static void hollow_rectangle(int totRows, int totCols){
+//         //Outer Loop
+//         for(int i=1; i<=totRows; i++){
+//             //inner-columns
+//             for(int j=1;j<=totCols;j++){
+//                 //cell-(i,j)
+//                 if(i==1||i==totRows||j==1||j==totCols){
+//                 //boundary cells
+//                 System.out.print("*");
+//                 }else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+//             public static void main(String args[]){
+//                 hollow_rectangle(7,10);
+//             }
+// }
+/*
+java -cp /tmp/YDyioyjYAF/HallowRectangle
+**********
+*        *
+*        *
+*        *
+*        *
+*        *
+**********
+
+=== Code Execution Successful ===
+
+
+
+*/
