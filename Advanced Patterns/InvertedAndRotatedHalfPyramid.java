788c07155e3f4a25d06be175421e6ff163b7466d
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-05-17 13:08:26 +0000

    InvertedAndRotatedHalfPyramid & HalfPyramid

---
+++ Advanced Patterns/InvertedAndRotatedHalfPyramid.java
@@ -0,0 +1,57 @@
+// import java.util.*;
+// public class InvertedAndRotatedHalfPyramid{
+//     public static void Inverted(int n){
+//         for(int i=1;i<=n;i++){
+//             //spaces
+//             for(int j=1;j<=n-i;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+        
+//     }
+//     public static void main(String args[]){
+//         Inverted(4);
+//     }
+// }
+/*OUTPUT:
+   *
+  **
+ ***
+****
+
+*/
+//CODE TO PRINT HALF PYRAMID 
+// import java.util.*;
+// public class InvertedAndRotatedHalfPyramid{
+//     public static void HalfPyramid(int n){
+//         for(int i=1;i<=n;i++){
+//             //spaces
+//             for(int j=1;j<=n-i;j++){
+//                 System.out.print("");
+//             }
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+        
+//     }
+//     public static void main(String args[]){
+//         Inverted(4);
+//     }
+// }
+
+
+/*
+OUTPUT:
+*
+**
+***
+****
+
+
+*/ 
